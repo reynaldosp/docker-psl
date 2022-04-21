d6b39710108b16b35a470df01f5b9a57f7dacb94
reynaldosp <reynaldo.pratama@tokopedia.com>
2022-04-21 14:37:26 +0700

    skeleton jenkins

--- src/com/workshop/Pipeline.groovy
+++ src/com/workshop/Pipeline.groovy
@@ -1,36 +1,30 @@
+#!/usr/bin/groovy
 package com.workshop
-
-class Pipeline implements Serializable {
-
-    String repository_name
-    String branch_name
-    String git_user
-    String app_port
-    String pr_num
-    String dockerTool
-    String merge_url
-    String docker_registry
-    String timeout_hc
-
-    Pipeline(
-        String repository_name,
-        String branch_name,
-        String git_user,
-        String app_port,
-        String pr_num,
-        String dockerTool,
-        String docker_registry,
-        String timeout_hc
-    ){
-        this.repository_name = repository_name
-        this.branch_name = branch_name
-        this.git_user = git_user
-        this.app_port = app_port
-        this.pr_num = pr_num
-        this.dockerTool = dockerTool
-        this.docker_registry = docker_registry
-        this.timeout_hc = timeout_hc
-    }
-
+ 
+class Pipeline {
+ 
+   String repository_name
+   String branch_name
+   String git_user
+   String docker_user
+   String app_port
+   String pr_num
+ 
+   Pipeline(
+       String repository_name,
+       String branch_name,
+       String git_user,
+       String docker_user,
+       String app_port,
+       String pr_num
+   ){
+       this.repository_name = repository_name
+       this.branch_name = branch_name
+       this.git_user = git_user
+       this.docker_user = docker_user
+       this.app_port = app_port
+       this.pr_num = pr_num
+   }
+ 
 }
 
--- src/com/workshop/main.groovy
+++ src/com/workshop/main.groovy
@@ -1,86 +1,60 @@
+
 #!/usr/bin/groovy
 package com.workshop
-
+ 
 import com.workshop.Config
-import com.workshop.utils
-import com.workshop.stages.*
-
-
+ 
 def main(script) {
-    // Object initialization
-    c = new Config()
-    u = new utils()
-    sprebuild = new prebuild()
-    sbuild = new build()
-    spostbuild = new postbuild()
-    sdeploy = new deploy()
-    spostdeploy = new postdeploy()
-
-    // Pipeline specific variable get from injected env
-    // Mandatory variable wil be check at details & validation steps
-    def repository_name = ("${script.env.repository_name}" != "null") ? "${script.env.repository_name}" : ""
-    def branch_name = ("${script.env.branch_name}" != "null") ? "${script.env.branch_name}" : ""
-    def git_user = ("${script.env.git_user}" != "null") ? "${script.env.git_user}" : ""
-    def app_port = ("${script.env.app_port}" != "null") ? "${script.env.app_port}" : ""
-    def pr_num = ("${script.env.pr_num}" != "null") ? "${script.env.pr_num}" : ""
-
-    // Timeout for Healtcheck
-    def timeout_hc = (script.env.timeout_hc != "null") ? script.env.timeout_hc : 10
-
-    // Have default value
-    def docker_registry = ("${script.env.docker_registry}" != "null") ? "${script.env.docker_registry}" : "${c.default_docker_registry}"
-
-    // Initialize docker tools
-    def dockerTool = tool name: 'docker', type: 'dockerTool'
-
-    p = new Pipeline(
-        repository_name,
-        branch_name,
-        git_user,
-        app_port,
-        pr_num,
-        dockerTool,
-        docker_registry,
-        timeout_hc
-    )
-
-    ansiColor('xterm') {
-        stage('Pre Build - Details') {
-            withCredentials([usernamePassword(credentialsId: 'dimasmamot-github-personal', passwordVariable: 'git_token', usernameVariable: 'git_username')]) {
-                // Check PR Merged atau belum
-                // Masukin ke variable
-            }
-
-            sprebuild.validation(p)
-            sprebuild.details(p)
-        }
-
-        stage('Pre Build - Checkout & Test') {
-            sprebuild.checkoutBuildTest(p)
-        }
-
-        stage('Build & Push Image') {
-            sbuild.build(p)
-        }
-
-        stage('Merge') {
-            withCredentials([usernamePassword(credentialsId: 'dimasmamot-github-personal', passwordVariable: 'git_token', usernameVariable: 'git_username')]) {
-                // Check pr merged atau belum dari variable is_merged
-                // Kalau sudah langsung deploy
-                // Kalau belum check apa merge nya udah berhasil atau ngga
-                // Kalau berhasil lanjut deploy
-                // Kalau gagal jangan lanjut
-            }
-        }
-
-        stage('Deploy') {
-            sdeploy.deploy(p)
-        }
-
-        stage('Service Healthcheck') {
-            spostdeploy.healthcheck(p)
-        }
-    }
+   // Object initialization
+   c = new Config()
+ 
+   // Pipeline specific variable get from injected env
+   // Mandatory variable will be check at details & validation steps
+   def repository_name = ("${script.env.repository_name}" != "null") ? "${script.env.repository_name}" : ""
+   def branch_name = ("${script.env.branch_name}" != "null") ? "${script.env.branch_name}" : ""
+   def git_user = ("${script.env.git_user}" != "null") ? "${script.env.git_user}" : ""
+   def docker_user = ("${script.env.docker_user}" != "null") ? "${script.env.docker_user}" : ""
+   def app_port = ("${script.env.app_port}" != "null") ? "${script.env.app_port}" : ""
+   def pr_num = ("${script.env.pr_num}" != "null") ? "${script.env.pr_num}" : ""
+ 
+   // Pipeline object
+   p = new Pipeline(
+       repository_name,
+       branch_name,
+       git_user,
+       docker_user,
+       app_port,
+       pr_num
+       
+ 
+   )
+ 
+   ansiColor('xterm') {
+       //stage('Pre Build - Details') {
+           // TODO: Call pre build details function
+       //}
+ 
+       //stage('Pre Build - Checkout & Test') {
+           // TODO: Call pre build checkout & test function
+       //}
+ 
+       //stage('Build & Push Image') {
+           // TODO: Call build & push image function
+       //}
+ 
+       //stage('Merge') {
+           // TODO: Call merge function
+       //}
+ 
+       //stage('Deploy') {
+           // TODO: Call deploy function
+       //}
+ 
+       //stage('Service Healthcheck') {
+           // TODO: Call healthcheck function
+       //}
+   }
 }
-
+ 
 return this
+
